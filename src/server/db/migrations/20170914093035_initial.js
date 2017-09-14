219379243fa2e7aeaa5028ea70e7adae5502bdd8
Jason Collins <the.oem@gmail.com>
2017-09-14 10:06:19 -0600

    Update to not require unique name of item.

--- src/server/db/migrations/20170914093035_initial.js
+++ src/server/db/migrations/20170914093035_initial.js
@@ -2,7 +2,7 @@ exports.up = function (knex, Promise) {
   return Promise.all([
     knex.schema.createTable('items', (table) => {
       table.increments('id').primary()
-      table.string('name').unique()
+      table.string('name')
       table.string('staleness_reason')
       table.string('cleanliness')
       table.timestamps(true, true)
