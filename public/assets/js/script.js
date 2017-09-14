ae5953b6d5b8a84652f1b052bf1b47393b0cda2f
Jason Collins <the.oem@gmail.com>
2017-09-14 15:05:36 -0600

    Implemented update cleanliness on items.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -36,13 +36,28 @@ const loadItemsInDom = (items) => {
 }
 
 const addItemInDom = (item) => {
+  console.log(item.cleanliness)
   $('.item-container').prepend(`
     <div class="item" id="${item.id}">
-      <div class="item-name"><strong>Name:</strong> ${item.name}</div>
-      <div class="item-explanation"><strong>Why is this here?</strong> ${item.staleness_reason}</div>
-      <div class="item-cleanliness"><strong>Condition:</strong> ${item.cleanliness}</div>
-    </div>
+      <div class="item-name"><strong>${item.name}</strong></div>
+      <div class="item-explanation hidden"><strong>Why is this here?</strong> ${item.staleness_reason}</div>
+      <div class="item-cleanliness hidden"><strong>Condition:</strong>
+        <select id="itemCondition">
+          <option value="Sparkling" ${checkSelected(item.cleanliness, 'Sparkling')}>Sparkling</option>
+          <option value="Dusty" ${checkSelected(item.cleanliness, 'Dusty')}>Dusty</option>
+          <option value="Rancid" ${checkSelected(item.cleanliness, 'Rancid')}>Rancid</option>
+        </select>
+      </div>
   `)
+}
+
+const updateCondition = (event) => {
+  const itemId = $(event.target).closest('.item').attr('id')
+  apiUpdateCondition(itemId, $(event.target)[0].value)
+}
+
+const checkSelected = (cleanliness, option) => {
+  return cleanliness === option ? 'selected' : ''
 }
 
 const updateItemCounts = items => {
@@ -56,8 +71,24 @@ const apiGetItems = () => fetch('/api/v1/items')
   .then(response => response.json())
   .catch(error => console.log(error))
 
+const apiUpdateCondition = (id, cleanliness) => {
+  fetch(`/api/v1/items/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ cleanliness })
+  })
+    .then((response) => {
+      if (!response.ok) throw Error(response.statusText)
+      return response.json()
+    })
+    .catch(error => console.log(error))
+}
+
 $('#asc').on('click', sortPageItems)
 $('#desc').on('click', sortPageItems)
+$('.item-container').on('change', '#itemCondition', updateCondition)
 $('.garage-door').click(() => {
   if ($('.garage-door').hasClass('slideup')) {
     $('.garage-door').removeClass('slideup').addClass('slidedown')
