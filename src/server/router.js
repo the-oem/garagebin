9d61ed0662d1c4627ffad38f2243e907862e538c
Jason Collins <the.oem@gmail.com>
2017-09-14 10:07:18 -0600

    Add method to POST a new item.

--- src/server/router.js
+++ src/server/router.js
@@ -5,5 +5,6 @@ const controller = require('./controller')
 
 router.get('/v1/items', controller.getItems)
 router.get('/v1/items/:id', controller.getItem)
+router.post('/v1/items', controller.addItem)
 
 module.exports = router
