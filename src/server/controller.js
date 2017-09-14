7c3686848fc64e28bc31c8db1739d1ed757b6c4f
Jason Collins <the.oem@gmail.com>
2017-09-14 09:47:02 -0600

    Add endpoint for all items. Supporting query params for filtering by cleanliness.

--- src/server/controller.js
+++ src/server/controller.js
@@ -1,9 +1,13 @@
 const DB = require('./db/knex')
 
 const getItems = (req, res) => {
-  res.status(200).json({
-      message: "Garage Bin has been opened!"
-    })
+  DB('items')
+    .where(req.query)
+    .select()
+    .then(items => res.status(200).json({
+      data: items
+    }))
+    .catch(error => res.status(500).json({ error }))
 }
 
 module.exports = {
