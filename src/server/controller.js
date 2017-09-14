57e440d1d5cc7c87178434f9fe6f1067e57655ab
Jason Collins <the.oem@gmail.com>
2017-09-14 09:53:54 -0600

    Add completed get item by ID endpoint.

--- src/server/controller.js
+++ src/server/controller.js
@@ -11,7 +11,15 @@ const getItems = (req, res) => {
 }
 
 const getItem = (req, res) => {
-  res.status(200).json({ message: 'Returning one item.' })
+  DB('items').where('id', parseInt(req.params.id, 10)).select()
+    .then(item => (item.length ? res.status(200).json({
+      data: item
+    }) : res.status(404).json({
+      data: {
+        error: `Item with id (${parseInt(req.params.id, 10)}) was not found.`
+      }
+    })))
+    .catch(error => res.status(500).json({ error }))
 }
 
 module.exports = {
