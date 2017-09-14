186707c7f50208f915300a5520f227f640003066
Jason Collins <the.oem@gmail.com>
2017-09-14 10:40:14 -0600

    Add tests for GET items endpoints.

--- test/integration/routes.test.js
+++ test/integration/routes.test.js
@@ -24,8 +24,57 @@ describe('Testing GarageBin API Routes', () => {
   })
 
   describe('GET /api/v1/items', () => {
-    it('does a thing', (done) => {
-      done()
+    it('should respond with all items in the garage', (done) => {
+      chai.request(server)
+      .get('/api/v1/items')
+      .end((err, res) => {
+        should.not.exist(err)
+        res.status.should.equal(200)
+        res.type.should.equal('application/json')
+        res.body.data[0].should.include.keys(
+          'id', 'name', 'staleness_reason', 'cleanliness')
+        done()
+      })
+    })
+
+    it('should respond with all items in the garage, filtered by a query param', (done) => {
+      chai.request(server)
+      .get('/api/v1/items?cleanliness=Dusty')
+      .end((err, res) => {
+        should.not.exist(err)
+        res.status.should.equal(200)
+        res.type.should.equal('application/json')
+        res.body.data.length.should.equal(3)
+        res.body.data[0].should.include.keys(
+          'id', 'name', 'staleness_reason', 'cleanliness')
+        done()
+      })
+    })
+
+    it('should respond with an empty array if no filtered items are found', (done) => {
+      chai.request(server)
+      .get('/api/v1/items?cleanliness=Clean')
+      .end((err, res) => {
+        should.not.exist(err)
+        res.status.should.equal(200)
+        res.type.should.equal('application/json')
+        res.body.data.length.should.equal(0)
+
+        done()
+      })
+    })
+
+    it('should respond with a 500 error if a filter query param is misspelled', (done) => {
+      chai.request(server)
+      .get('/api/v1/items?cleaniness=Dusty')
+      .end((err, res) => {
+        should.exist(err)
+        res.status.should.equal(500)
+        res.type.should.equal('application/json')
+        res.body.error.severity.should.equal('ERROR')
+        res.body.error.routine.should.equal('errorMissingColumn')
+        done()
+      })
     })
   })
 })
